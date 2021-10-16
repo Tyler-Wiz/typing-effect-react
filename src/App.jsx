d0635c81f1cca5622252321fb1bff43fb93212d3
Tyler <tylertooxclusive@gmail.com>
2021-10-16 18:11:11 +0100

    typewriter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import  './App.css'
+import Typewriter from 'typewriter-effect';
 
 function App() {
   return (
-    <div>
-      <h1 className="typing">Hello World</h1>
+    <div className="typing">
+      <Typewriter
+        options={{
+        strings: [
+          'Web Developer From Cambridge', 
+          'UX Designer'
+        ],
+        cursorClassName:'blink',
+        autoStart: true,
+        loop: true,
+        }}
+      />
     </div>
   )
 }
